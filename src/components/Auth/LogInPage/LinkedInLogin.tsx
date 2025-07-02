ab64b43fb3ba32e51dd88571fcd4ddf2d602a580
rayhan108 <raihanshorker56@gmail.com>
2025-07-02 14:16:03 +0600

    fix linedin login btn

--- src/components/Auth/LogInPage/LinkedInLogin.tsx
+++ src/components/Auth/LogInPage/LinkedInLogin.tsx
@@ -1,16 +1,16 @@
 import { AllImages } from '@/assets/AllImages';
-import { Button } from '@/components/ui/button';
+
 import Image from 'next/image';
 import React from 'react';
 
 const LinkedInLogin = () => {
     return (
         <div>
-            <Button className="flex items-center justify-center gap-4 border py-4 px-32 rounded-md text-sm font-semibold">
+            <button className="flex items-center justify-center gap-4 border py-2 px-8 w-full bg-[#EAB030] rounded-md text-sm font-semibold">
                 {" "}
                 <Image src={AllImages.linkedinIcon} alt="linkedin" /> Sign Up with
                 Linkedin
-            </Button>
+            </button>
         </div>
     );
 };
